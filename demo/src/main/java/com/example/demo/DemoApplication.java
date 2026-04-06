29667d9bc408138a815b68be3b3b4215213ed935
aiaod1009 <3449508132@qq.com>
2026-04-06 23:30:11 +0800

    连数据库

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,12 +7,19 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 import com.example.demo.config.AIConfig;
 
-@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+// @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+// @EnableConfigurationProperties
+// public class DemoApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(DemoApplication.class, args);
+// 	}
+
+// }
+@SpringBootApplication
 @EnableConfigurationProperties
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
